c725ab437b6b696e68e3a676120b5d5e59add559
Jeremy D. Frens <jdfrens@gmail.com>
2009-11-14 15:43:54 -0600

    Type-checking division done for interpreter in less than a minute.

--- tests/org/norecess/hobbes/interpreter/operators/DivisionAppliableTest.java
+++ tests/org/norecess/hobbes/interpreter/operators/DivisionAppliableTest.java
@@ -2,9 +2,13 @@ package org.norecess.hobbes.interpreter.operators;
 
 import static org.junit.Assert.assertEquals;
 
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
 import org.junit.Before;
 import org.junit.Test;
+import org.norecess.citkit.tir.data.DatumTIR;
 import org.norecess.citkit.tir.expressions.IntegerETIR;
+import org.norecess.hobbes.typechecker.OperatorTypeException;
 
 public class DivisionAppliableTest {
 
@@ -21,6 +25,16 @@ public class DivisionAppliableTest {
 				new IntegerETIR(2)));
 		assertEquals(new IntegerETIR(2), myOperator.apply(new IntegerETIR(15),
 				new IntegerETIR(6)));
+	}
+
+	@Test(expected = OperatorTypeException.class)
+	public void shouldTypeCheck() {
+		IMocksControl myMocksControl = EasyMock.createControl();
+		DatumTIR datum1 = myMocksControl.createMock(DatumTIR.class);
+		DatumTIR datum2 = myMocksControl.createMock(DatumTIR.class);
+
+		myMocksControl.replay();
+		myOperator.apply(datum1, datum2);
 	}
 
 }
