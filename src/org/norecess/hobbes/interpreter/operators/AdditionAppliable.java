5618e292c981e82134d025fd1d075a66a1f8703a
Jeremy D. Frens <jdfrens@gmail.com>
2009-11-14 13:59:21 -0600

    Addition expressions are type checked in the interpreter.

--- src/org/norecess/hobbes/interpreter/operators/AdditionAppliable.java
+++ src/org/norecess/hobbes/interpreter/operators/AdditionAppliable.java
@@ -3,11 +3,13 @@ package org.norecess.hobbes.interpreter.operators;
 import org.norecess.citkit.tir.data.DatumTIR;
 import org.norecess.citkit.tir.expressions.IIntegerETIR;
 import org.norecess.citkit.tir.expressions.IntegerETIR;
+import org.norecess.hobbes.interpreter.HobbesTypeException;
 
 public class AdditionAppliable implements Appliable {
 
 	public DatumTIR apply(DatumTIR i, DatumTIR j) {
-		throw new IllegalStateException("unimplemented!");
+		throw new HobbesTypeException(i.getType().toShortString() + " + "
+				+ j.getType().toShortString());
 	}
 
 	public IIntegerETIR apply(IIntegerETIR i, IIntegerETIR j) {
--- tests/org/norecess/hobbes/interpreter/operators/AdditionAppliableTest.java
+++ tests/org/norecess/hobbes/interpreter/operators/AdditionAppliableTest.java
@@ -1,10 +1,16 @@
 package org.norecess.hobbes.interpreter.operators;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
 import org.junit.Before;
 import org.junit.Test;
+import org.norecess.citkit.tir.data.DatumTIR;
 import org.norecess.citkit.tir.expressions.IntegerETIR;
+import org.norecess.citkit.types.HobbesType;
+import org.norecess.hobbes.interpreter.HobbesTypeException;
 
 public class AdditionAppliableTest {
 
@@ -16,11 +22,34 @@ public class AdditionAppliableTest {
 	}
 
 	@Test
-	public void shouldInterpretMultiplication() {
+	public void shouldInterpretAddition() {
 		assertEquals(new IntegerETIR(10), myOperator.apply(new IntegerETIR(2),
 				new IntegerETIR(8)));
 		assertEquals(new IntegerETIR(5), myOperator.apply(new IntegerETIR(15),
 				new IntegerETIR(-10)));
+	}
+
+	@Test
+	public void shouldTypeCheck() {
+		IMocksControl myMocksControl = EasyMock.createControl();
+		DatumTIR datum1 = myMocksControl.createMock(DatumTIR.class);
+		DatumTIR datum2 = myMocksControl.createMock(DatumTIR.class);
+		HobbesType type1 = myMocksControl.createMock(HobbesType.class);
+		HobbesType type2 = myMocksControl.createMock(HobbesType.class);
+
+		EasyMock.expect(datum1.getType()).andReturn(type1);
+		EasyMock.expect(type1.toShortString()).andReturn("TypeA");
+		EasyMock.expect(datum2.getType()).andReturn(type2);
+		EasyMock.expect(type2.toShortString()).andReturn("TypeB");
+
+		myMocksControl.replay();
+		try {
+			myOperator.apply(datum1, datum2);
+			fail("should be a type exception");
+		} catch (HobbesTypeException e) {
+			assertEquals("TypeA + TypeB", e.getMessage());
+		}
+		myMocksControl.verify();
 	}
 
 }
